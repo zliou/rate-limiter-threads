ca6289b291384701b7d2d60d528218ef4a776d45
Zach Liou <zachliou@gmail.com>
2023-10-23 18:31:23 -0400

    Test thread printers

--- main.cc
+++ main.cc
@@ -1,13 +1,28 @@
 #include <iostream>
 #include <thread>
+#include <vector>
 
-void PrintRange(int start, int end) {
+
+void PrintRange(int thread_number, int start, int end) {
   for (int i = start; i < end; ++i) {
-    std::cout << i << std::endl;
+    std::cout << "Thread " << thread_number << ": " << i << std::endl;
   }
 }
 
 int main() {
+  int num_threads = 20;
+  std::vector<std::thread> threads(num_threads);
+
+  // Initialize threads.
+  for (size_t i = 0; i < num_threads; ++i) {
+    threads[i] = std::thread(PrintRange, i, 0, 9);
+  }
+
+  // Join threads.
+  for (std::thread& t : threads) {
+    t.join();
+  }
+
   return 0;
 }
 
